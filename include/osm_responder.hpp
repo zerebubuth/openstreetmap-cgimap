0a4611d7741cf4804224becca04e01cfaf88fb50
Matt Amos <zerebubuth@gmail.com>
2011-02-25 23:56:06 +0000

    Added some documentation to OSM responder.

--- include/osm_responder.hpp
+++ include/osm_responder.hpp
@@ -7,14 +7,37 @@
 #include <pqxx/pqxx>
 #include <boost/optional.hpp>
 
+/**
+ * utility class - use this as a base class when the derived class is going to
+ * respond in OSM format (i.e: nodes, ways and relations). this class will take
+ * care of the write() and types_available() methods, allowing derived code to
+ * be more concise.
+ *
+ * if you want a <bounds> element to be written, include the bounds constructor
+ * argument. otherwise leave it out and it'll default to no bounds element.
+ */
 class osm_responder : public responder {
 public:
+	 // construct, passing the mime type down to the responder. the current transaction 
+   // optional bounds are stored at this level, but available to derived classes.
 	 osm_responder(mime::type, pqxx::work &, boost::optional<bbox> bounds = boost::optional<bbox>());
+
 	 virtual ~osm_responder() throw();
+
+	 // lists the standard types that OSM format can respond in, currently XML and, if 
+   // the yajl library is provided, JSON.
 	 std::list<mime::type> types_available() const;
+
+	 // writes whatever is in the tmp_nodes/ways/relations tables to the given 
+	 // formatter.
 	 void write(std::auto_ptr<output_formatter> f);
+
 protected:
+	 // current transaction
 	 pqxx::work &w;
+
+	 // optional bounds element - this is only for information and has no effect on 
+   // behaviour other than whether the bounds element gets written.
 	 boost::optional<bbox> bounds;
 };
 
