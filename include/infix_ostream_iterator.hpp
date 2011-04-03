9c17759a222b61b3bf0aa43ae0d8296d4571b3f5
Kai Krueger <kai@aiputerlx.(none)>
2011-04-03 16:04:34 -0600

    Replace infix_ostream_iterator.hpp with a different version
    The current version appears to have missed the delimiter between list elements

--- include/infix_ostream_iterator.hpp
+++ include/infix_ostream_iterator.hpp
@@ -1,44 +1,55 @@
-#ifndef INFIX_OSTREAM_ITERATOR
-#define INFIX_OSTREAM_ITERATOR
+// Warning: only minimally tested.
+// infix_iterator.h
+// Lifted from Jerry Coffin's 's prefix_ostream_iterator
+#if !defined(INFIX_ITERATOR_H_)
+#define  INFIX_ITERATOR_H_
 
-#include <ios>
+
 #include <ostream>
 #include <iterator>
 
-/**
- * output iterator which infixes its delimiter string between the output items.
- */
-template <typename _Tp, typename _CharT = char, typename _Traits = std::char_traits<_CharT> >
-class infix_ostream_iterator : public std::iterator<std::output_iterator_tag, void, void, void, void> {
-public:
-	 typedef _CharT char_type;
-	 typedef _Traits traits_type;
-	 typedef std::basic_ostream<_CharT, _Traits> ostream_type;
+template <class T,
+class charT=char,
+class traits=std::char_traits<charT> >
 
-private:
-	 ostream_type *_M_stream;
-	 const _CharT *_M_string;
-	 bool _M_first;
+class infix_ostream_iterator :
 
+        public std::iterator
+<std::output_iterator_tag,void,void,void,void> {
+
+	std::basic_ostream<charT,traits> *os;
+	charT const* delimiter;
+	bool first_elem;
 public:
-	 infix_ostream_iterator(ostream_type &s) : _M_stream(&s), _M_string(0), _M_first(true) {}
-	 infix_ostream_iterator(ostream_type &s, const _CharT *c) : _M_stream(&s), _M_string(c), _M_first(true) {}
-	 infix_ostream_iterator(const infix_ostream_iterator &obj) : _M_stream(obj._M_stream), _M_string(obj._M_string), _M_first(obj._M_first) {}
-	
-	 infix_ostream_iterator &operator=(const _Tp &value) {
-		 __glibcxx_requires_cond(_M_stream != 0, _M_message(__gnu_debug::__msg_output_ostream)._M_iterator(*this));
-		 if ((_M_string != 0) && (_M_first == false)) {
-			 *_M_stream << _M_string;
-		 } else {
-			 _M_first = true;
-		 }
-		 *_M_stream << value;
-		 return *this;
-	 }
- 
-	 infix_ostream_iterator &operator*() { return *this; }
-	 infix_ostream_iterator &operator++() { return *this; }
-	 infix_ostream_iterator &operator++(int) { return *this; }
+	typedef charT char_type;
+	typedef traits traits_type;
+	typedef std::basic_ostream<charT,traits> ostream_type;
+
+	infix_ostream_iterator(ostream_type& s)
+		: os(&s),delimiter(0), first_elem(true)
+	{}
+	infix_ostream_iterator(ostream_type& s, charT const *d)
+		: os(&s),delimiter(d), first_elem(true)
+	{}
+	infix_ostream_iterator<T,charT,traits>& operator=(T const &item)
+	{
+		if (!first_elem && delimiter != 0)
+			*os << delimiter;
+		*os << item;
+		first_elem = false;
+		return *this;
+	}
+
+	infix_ostream_iterator<T,charT,traits> &operator*() {
+		return *this;
+	}
+	infix_ostream_iterator<T,charT,traits> &operator++() {
+		return *this;
+	}
+	infix_ostream_iterator<T,charT,traits> &operator++(int) {
+		return *this;
+	}
+
+
 };
-
-#endif /* INFIX_OSTREAM_ITERATOR */
+#endif 
