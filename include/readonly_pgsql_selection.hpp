92000b5fdab31f440ce5e424741229df4743ad88
Matt Amos <zerebubuth@gmail.com>
2011-09-14 05:04:33 +0100

    Added true read-only postgresql backend, suitable for use on replicating slaves.

---
+++ include/readonly_pgsql_selection.hpp
@@ -0,0 +1,54 @@
+#ifndef READONLY_PGSQL_SELECTION_HPP
+#define READONLY_PGSQL_SELECTION_HPP
+
+#include "data_selection.hpp"
+#include <set>
+
+/**
+ * a selection which operates against a readonly (e.g: replicating
+ * slave) PostgreSQL database. This is needed because, currently, 
+ * slaves can't create XIDs which would be necessary to create 
+ * temporary tables. Instead, the state of the selection must be
+ * stored in-memory in cgimap's memory.
+ */
+class readonly_pgsql_selection
+	: public data_selection {
+public:
+	 readonly_pgsql_selection(pqxx::work &w_);
+	 ~readonly_pgsql_selection();
+
+	 void write_nodes(output_formatter &formatter);
+	 void write_ways(output_formatter &formatter);
+	 void write_relations(output_formatter &formatter);
+
+	 int num_nodes();
+	 int num_ways();
+	 int num_relations();
+	 visibility_t check_node_visibility(id_t id);
+	 visibility_t check_way_visibility(id_t id);
+	 visibility_t check_relation_visibility(id_t id);
+
+	 void select_visible_nodes(const std::list<id_t> &);
+	 void select_visible_ways(const std::list<id_t> &);
+	 void select_visible_relations(const std::list<id_t> &);
+	 void select_nodes_from_bbox(const bbox &bounds, int max_nodes);
+	 void select_nodes_from_relations();
+	 void select_ways_from_nodes();
+	 void select_ways_from_relations();
+	 void select_relations_from_ways();
+	 void select_nodes_from_way_nodes();
+	 void select_relations_from_nodes();
+	 void select_relations_from_way_nodes();
+	 void select_relations_from_relations();
+
+private:
+	 // the transaction in which the selection takes place. this is
+	 // fully read-only, and cannot create any temporary tables, 
+	 // unlike writeable_pgsql_selection.
+	 pqxx::work &w;
+
+	 // the set of selected nodes, ways and relations
+	 std::set<id_t> sel_nodes, sel_ways, sel_relations;
+};
+
+#endif /* READONLY_PGSQL_SELECTION_HPP */
---
+++ src/readonly_pgsql_selection.cpp
@@ -0,0 +1,376 @@
+#include "readonly_pgsql_selection.hpp"
+#include "logger.hpp"
+#include "quad_tile.hpp"
+#include "temp_tables.hpp"
+#include "infix_ostream_iterator.hpp"
+
+#include <sstream>
+#include <list>
+
+using std::set;
+using std::stringstream;
+using std::list;
+
+// number of nodes to chunk together
+#define STRIDE (1000)
+
+namespace {
+inline data_selection::visibility_t 
+check_table_visibility(pqxx::work &w, id_t id, const char *table) {
+	stringstream query;
+	query << "select visible from current_" << table << "s where id = " << id;
+	pqxx::result res = w.exec(query);
+
+	if (res.size() > 0) {
+		if (res[0][0].as<bool>()) {
+			return data_selection::exists;
+		} else {
+			return data_selection::deleted;
+		}
+	} else {
+		return data_selection::non_exist;
+	}	
+}
+
+inline void
+insert_results_of(pqxx::work &w, std::stringstream &query, set<id_t> &elems) {
+	pqxx::result res = w.exec(query);
+
+	for (pqxx::result::const_iterator itr = res.begin(); 
+			 itr != res.end(); ++itr) {
+		const long int id = (*itr)["id"].as<long int>();
+		elems.insert(id);
+	}
+}
+} // anonymous namespace
+
+readonly_pgsql_selection::readonly_pgsql_selection(pqxx::work &w_)
+	: w(w_) {
+}
+
+readonly_pgsql_selection::~readonly_pgsql_selection() {
+}
+
+void 
+readonly_pgsql_selection::write_nodes(output_formatter &formatter) {
+  // get all nodes - they already contain their own tags, so
+  // we don't need to do anything else.
+  logger::message("Fetching nodes");
+
+  formatter.start_element_type(element_type_node, num_nodes());
+	// fetch in chunks...
+	set<id_t>::iterator prev_itr = sel_nodes.begin();
+	size_t chunk_i = 0;
+	for (set<id_t>::iterator n_itr = sel_nodes.begin();
+			 ; ++n_itr, ++chunk_i) {
+		bool at_end = n_itr == sel_nodes.end();
+		if ((chunk_i >= STRIDE) || at_end) {
+			stringstream query;
+			query << "select n.id, n.latitude, n.longitude, n.visible, "
+				 "to_char(n.timestamp,'YYYY-MM-DD\"T\"HH24:MI:SS\"Z\"') as timestamp, "
+				 "n.changeset_id, n.version from current_nodes n where n.id in (";
+			std::copy(prev_itr, n_itr, infix_ostream_iterator<id_t>(query, ","));
+			query << ")";
+			pqxx::result nodes = w.exec(query);
+				
+			for (pqxx::result::const_iterator itr = nodes.begin(); 
+					 itr != nodes.end(); ++itr) {
+				const long int id = (*itr)["id"].as<long int>();
+				pqxx::result tags = w.exec("select k, v from current_node_tags where id=" + pqxx::to_string(id));
+				formatter.write_node(*itr, tags);
+			}
+
+			if (at_end) break;
+
+			chunk_i = 0;
+			prev_itr = n_itr;
+		}
+  }
+  formatter.end_element_type(element_type_node);
+}
+
+void 
+readonly_pgsql_selection::write_ways(output_formatter &formatter) {
+  // grab the ways, way nodes and tags
+  // way nodes and tags are on a separate connections so that the
+  // entire result set can be streamed from a single query.
+  logger::message("Fetching ways");
+
+  formatter.start_element_type(element_type_way, num_ways());
+	// fetch in chunks...
+	set<id_t>::iterator prev_itr = sel_ways.begin();
+	size_t chunk_i = 0;
+	for (set<id_t>::iterator n_itr = sel_ways.begin();
+			 ; ++n_itr, ++chunk_i) {
+		bool at_end = n_itr == sel_ways.end();
+		if ((chunk_i >= STRIDE) || at_end) {
+			stringstream query;
+      query << "select w.id, w.visible, w.version, w.changeset_id, "
+				 "to_char(w.timestamp,'YYYY-MM-DD\"T\"HH24:MI:SS\"Z\"') as timestamp from "
+				 "current_ways w where w.visible = true and w.id in (";
+			std::copy(prev_itr, n_itr, infix_ostream_iterator<id_t>(query, ","));
+			query << ")";
+			pqxx::result ways = w.exec(query);
+
+			for (pqxx::result::const_iterator itr = ways.begin(); 
+					 itr != ways.end(); ++itr) {
+				const long int id = (*itr)["id"].as<long int>();
+				pqxx::result nodes = w.exec("select node_id from current_way_nodes where id=" + 
+																		pqxx::to_string(id) + " order by sequence_id asc");
+				pqxx::result tags = w.exec("select k, v from current_way_tags where id=" + pqxx::to_string(id));
+				formatter.write_way(*itr, nodes, tags);
+			}
+			
+			if (at_end) break;
+
+			chunk_i = 0;
+			prev_itr = n_itr;
+		}
+  }
+  formatter.end_element_type(element_type_way);
+}
+
+void 
+readonly_pgsql_selection::write_relations(output_formatter &formatter) {
+  logger::message("Fetching relations");
+
+  formatter.start_element_type(element_type_relation, num_relations());
+	// fetch in chunks...
+	set<id_t>::iterator prev_itr = sel_relations.begin();
+	size_t chunk_i = 0;
+	for (set<id_t>::iterator n_itr = sel_relations.begin();
+			 ; ++n_itr, ++chunk_i) {
+		bool at_end = n_itr == sel_relations.end();
+		if ((chunk_i >= STRIDE) || at_end) {
+			stringstream query;
+      query << "select r.id, r.visible, r.version, r.changeset_id, "
+				 "to_char(r.timestamp,'YYYY-MM-DD\"T\"HH24:MI:SS\"Z\"') as timestamp from "
+				 "current_relations r where r.visible = true and r.id in (";
+			std::copy(prev_itr, n_itr, infix_ostream_iterator<id_t>(query, ","));
+			query << ")";
+			pqxx::result relations = w.exec(query);
+			
+			for (pqxx::result::const_iterator itr = relations.begin(); 
+					 itr != relations.end(); ++itr) {
+				const long int id = (*itr)["id"].as<long int>();
+				pqxx::result members = w.exec("select member_type, member_id, member_role from "
+																			"current_relation_members where id=" + 
+																			pqxx::to_string(id) + " order by sequence_id asc");
+				pqxx::result tags = w.exec("select k, v from current_relation_tags where id=" + pqxx::to_string(id));
+				formatter.write_relation(*itr, members, tags);
+			}
+			
+			if (at_end) break;
+			
+			chunk_i = 0;
+			prev_itr = n_itr;
+		}
+  }
+  formatter.end_element_type(element_type_relation);
+}
+
+int 
+readonly_pgsql_selection::num_nodes() {
+  return sel_nodes.size();
+}
+
+int 
+readonly_pgsql_selection::num_ways() {
+	return sel_ways.size();
+}
+
+int 
+readonly_pgsql_selection::num_relations() {
+	return sel_relations.size();
+}
+
+data_selection::visibility_t 
+readonly_pgsql_selection::check_node_visibility(id_t id) {
+	return check_table_visibility(w, id, "node");
+}
+
+data_selection::visibility_t 
+readonly_pgsql_selection::check_way_visibility(id_t id) {
+	return check_table_visibility(w, id, "way");
+}
+
+data_selection::visibility_t 
+readonly_pgsql_selection::check_relation_visibility(id_t id) {
+	return check_table_visibility(w, id, "relation");
+}
+
+void
+readonly_pgsql_selection::select_visible_nodes(const std::list<id_t> &ids) {
+	stringstream query;
+	list<id_t>::const_iterator it;
+	
+	query << "select id from current_nodes where id IN (";
+	std::copy(ids.begin(), ids.end(), infix_ostream_iterator<id_t>(query, ","));
+	query << ") and visible";
+
+	insert_results_of(w, query, sel_nodes);
+}
+
+void
+readonly_pgsql_selection::select_visible_ways(const std::list<id_t> &ids) {
+	stringstream query;
+	list<id_t>::const_iterator it;
+	
+	query << "select id from current_ways where id IN (";
+	std::copy(ids.begin(), ids.end(), infix_ostream_iterator<id_t>(query, ","));
+	query << ") and visible";
+
+	insert_results_of(w, query, sel_ways);
+}
+
+void
+readonly_pgsql_selection::select_visible_relations(const std::list<id_t> &ids) {
+	stringstream query;
+	list<id_t>::const_iterator it;
+	
+	query << "select id from current_relations where id IN (";
+	std::copy(ids.begin(), ids.end(), infix_ostream_iterator<id_t>(query, ","));
+	query << ") and visible";
+
+	insert_results_of(w, query, sel_relations);
+}
+
+void 
+readonly_pgsql_selection::select_nodes_from_bbox(const bbox &bounds, int max_nodes) {
+  const set<unsigned int> tiles = 
+		 tiles_for_area(bounds.minlat, bounds.minlon, 
+										bounds.maxlat, bounds.maxlon);
+  
+  // hack around problem with postgres' statistics, which was 
+  // making it do seq scans all the time on smaug...
+  w.exec("set enable_mergejoin=false");
+  w.exec("set enable_hashjoin=false");
+	
+  stringstream query;
+  query << "select id from current_nodes where ((";
+  unsigned int first_id = 0, last_id = 0;
+  for (set<unsigned int>::const_iterator itr = tiles.begin();
+       itr != tiles.end(); ++itr) {
+    if (first_id == 0) {
+      last_id = first_id = *itr;
+    } else if (*itr == last_id + 1) {
+      ++last_id;
+    } else {
+      if (last_id == first_id) {
+				query << "tile = " << last_id << " or ";
+      } else {
+				query << "tile between " << first_id 
+							<< " and " << last_id << " or ";
+      }
+      last_id = first_id = *itr;
+    }
+  }
+  if (last_id == first_id) {
+    query << "tile = " << last_id << ") ";
+  } else {
+    query << "tile between " << first_id 
+					<< " and " << last_id << ") ";
+  }
+  query << "and latitude between " << int(bounds.minlat * SCALE) 
+				<< " and " << int(bounds.maxlat * SCALE)
+				<< " and longitude between " << int(bounds.minlon * SCALE) 
+				<< " and " << int(bounds.maxlon * SCALE)
+				<< ") and (visible = true)"
+				<< " limit " << (max_nodes + 1); // limit here as a quick hack to reduce load...
+
+  logger::message("Filling sel_nodes from bbox");
+  logger::message(query.str());
+
+	insert_results_of(w, query, sel_nodes);
+}
+
+void 
+readonly_pgsql_selection::select_nodes_from_relations() {
+  logger::message("Filling sel_nodes (from relations)");
+
+	stringstream query;
+	query << "select distinct rm.member_id as id from "
+		 "current_relation_members rm where rm.member_type='Node'"
+		 "and rm.id in (";
+	std::copy(sel_relations.begin(), sel_relations.end(), infix_ostream_iterator<id_t>(query, ","));	
+	query << ")";
+	insert_results_of(w, query, sel_nodes);
+}
+
+void 
+readonly_pgsql_selection::select_ways_from_nodes() {
+  logger::message("Filling sel_ways (from nodes)");
+
+	stringstream query;
+	query << "select distinct wn.id from current_way_nodes wn "
+		 "where wn.id in (";
+	std::copy(sel_nodes.begin(), sel_nodes.end(), infix_ostream_iterator<id_t>(query, ","));	
+	query << ")";
+	insert_results_of(w, query, sel_ways);
+}
+
+void 
+readonly_pgsql_selection::select_ways_from_relations() {
+  logger::message("Filling sel_ways (from relations)");
+
+	stringstream query;
+	query << "select distinct rm.member_id as id from "
+		 "current_relation_members rm where rm.member_type='Way' "
+		 "and rm.member_id in (";
+	std::copy(sel_relations.begin(), sel_relations.end(), infix_ostream_iterator<id_t>(query, ","));
+	query << ")";
+	insert_results_of(w, query, sel_ways);
+}
+
+void 
+readonly_pgsql_selection::select_relations_from_ways() {
+  logger::message("Filling sel_relations (from ways)");
+
+	stringstream query;
+	query << "select distinct id from current_relation_members rm where rm.member_type='Way' "
+		 "and rm.member_id in (";
+	std::copy(sel_ways.begin(), sel_ways.end(), infix_ostream_iterator<id_t>(query, ","));
+	query << ")";
+	insert_results_of(w, query, sel_relations);
+}
+
+void 
+readonly_pgsql_selection::select_nodes_from_way_nodes() {
+	stringstream query;
+	query << "select distinct wn.node_id as id from current_way_nodes wn "
+		 "where wn.id in ("; 
+	std::copy(sel_ways.begin(), sel_ways.end(), infix_ostream_iterator<id_t>(query, ","));
+	query << ")";
+	insert_results_of(w, query, sel_nodes);
+}
+
+void 
+readonly_pgsql_selection::select_relations_from_nodes() {
+	stringstream query;
+	query << "select distinct rm.id from current_relation_members rm "
+		 "where rm.member_type='Node' and rm.member_id in (";
+	std::copy(sel_nodes.begin(), sel_nodes.end(), infix_ostream_iterator<id_t>(query, ","));
+	query << ")";
+	insert_results_of(w, query, sel_relations);
+}
+
+void 
+readonly_pgsql_selection::select_relations_from_way_nodes() {
+	stringstream query;
+	query << "select distinct id from current_relation_members rm "
+		 "where rm.member_type='Node' and rm.member_id in (select distinct "
+		 "node_id from current_way_nodes where id in (";
+	std::copy(sel_ways.begin(), sel_ways.end(), infix_ostream_iterator<id_t>(query, ","));
+	query << ")";
+	insert_results_of(w, query, sel_relations);
+}
+
+void 
+readonly_pgsql_selection::select_relations_from_relations() {
+	stringstream query;
+	query << "select distinct id from current_relation_members rm "
+		 "where rm.member_type='Relation' and rm.member_id in (";
+	std::copy(sel_relations.begin(), sel_relations.end(), infix_ostream_iterator<id_t>(query, ","));
+	query << ")";
+	insert_results_of(w, query, sel_relations);
+}
