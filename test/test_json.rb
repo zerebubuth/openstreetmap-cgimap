7464759d3b04b1c09bab9e936d2154b3e5c12080
Matt Amos <zerebubuth@gmail.com>
2013-09-04 13:09:24 +0100

    Added another JSON test.
    It seems better to have empty objects for tags, and empty arrays
    for nodes/ways/relations than to omit these, as it leads to more
    predictable output and less complexity in the client parser.
    
    This commit adds a simple test to check that these keys are
    always present.

--- test/test_json.rb
+++ test/test_json.rb
@@ -29,6 +29,26 @@ test_request("GET", "/api/0.6/node/1", "HTTP_ACCEPT" => "text/json") do |headers
   assert(node["version"], 1, "Version attribute")
   assert(node["changeset"], 1, "Changeset ID")
   assert(node["timestamp"], "2012-09-25T00:00:00Z", "Timestamp")
+  # node 1 has no tags, but should still have "tags" object which
+  # will be empty.
+  assert(node.has_key?("tags"), true, "Node without tags has key 'tags'.")
+  assert(node["tags"].empty?, true, "Node without tags has empty 'tags' object.")
+end
+
+# grab an empty bounding box and check it has empty nodes, ways &
+# relations arrays.
+test_request("GET", "/api/0.6/map?bbox=10,10,10.1,10.1", "HTTP_ACCEPT" => "text/json") do |headers, data|
+  assert(headers["Status"], "200 OK", "Response status code.")
+  assert(headers["Content-Type"], "text/json; charset=utf-8", "Response content type.")
+
+  doc = JSON.load(data)
+
+  ['nodes', 'ways', 'relations'].each do |name|
+    assert(doc.has_key?(name), true, "Document has a '#{name}' key.")
+    array = doc[name]
+    assert(array.class, Array, "Element '#{name}' of document is an array.")
+    assert(array.size, 0, "Number of #{name}s.")
+  end
 end
 
 # TODO - add more tests!
