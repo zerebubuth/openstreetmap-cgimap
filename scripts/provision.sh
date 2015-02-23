6b424d7fd988556b987baef4380ddcab09b4c3d7
Matt Amos <zerebubuth@gmail.com>
2015-02-23 19:59:34 +0000

    Updated vagrant provisioning to match README.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-apt-get install -y git build-essential automake autoconf;
+apt-get install -y git build-essential automake autoconf libtool;
 
 apt-get install -y libxml2-dev libpqxx3-dev libfcgi-dev \
   libboost-dev libboost-regex-dev libboost-program-options-dev \
-  libboost-date-time-dev libboost-system-dev libmemcached-dev;
+  libboost-date-time-dev libboost-filesystem-dev \
+  libboost-system-dev libmemcached-dev;
 
 # following stuff is for testing only
 apt-get install -y postgresql postgresql-contrib postgis \
