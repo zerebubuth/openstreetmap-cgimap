701caa6244ad2b15760ea361817f8793c44b351c
Matt Amos <zerebubuth@gmail.com>
2016-08-22 11:15:06 +0100

    Replace hand-written parser with call to library function.

--- src/time.cpp
+++ src/time.cpp
@@ -4,50 +4,26 @@
 #include <sstream>
 #include <ctype.h>
 
-boost::posix_time::ptime parse_time(const std::string &s) {
+namespace bpt = boost::posix_time;
+namespace bdt = boost::date_time;
+
+bpt::ptime parse_time(const std::string &s) {
   // parse only YYYY-MM-DDTHH:MM:SSZ
-  if ((s.size() == 20) &&
-      (isdigit(s[0]) != 0) &&
-      (isdigit(s[1]) != 0) &&
-      (isdigit(s[2]) != 0) &&
-      (isdigit(s[3]) != 0) &&
-      (s[4] == '-') &&
-      (isdigit(s[5]) != 0) &&
-      (isdigit(s[6]) != 0) &&
-      (s[7] == '-') &&
-      (isdigit(s[8]) != 0) &&
-      (isdigit(s[9]) != 0) &&
-      (s[10] == 'T') &&
-      (isdigit(s[11]) != 0) &&
-      (isdigit(s[12]) != 0) &&
-      (s[13] == ':') &&
-      (isdigit(s[14]) != 0) &&
-      (isdigit(s[15]) != 0) &&
-      (s[16] == ':') &&
-      (isdigit(s[17]) != 0) &&
-      (isdigit(s[18]) != 0) &&
-      (s[19] == 'Z')) {
-    return boost::posix_time::ptime(
-      boost::gregorian::date(
-        int(s[0] - '0') * 1000 +
-        int(s[1] - '0') *  100 +
-        int(s[2] - '0') *   10 +
-        int(s[3] - '0'),
-        int(s[5] - '0') *   10 +
-        int(s[6] - '0'),
-        int(s[8] - '0') *   10 +
-        int(s[9] - '0')),
-      boost::posix_time::time_duration(
-        int(s[11] - '0') *   10 +
-        int(s[12] - '0'),
-        int(s[14] - '0') *   10 +
-        int(s[15] - '0'),
-        int(s[17] - '0') *   10 +
-        int(s[18] - '0')));
+  if ((s.size() == 20) && (s[19] == 'Z')) {
+    // chop off the trailing 'Z' so as not to interfere with the standard
+    // parsing.
+    std::string without_tz = s.substr(0, 19);
+
+    bpt::ptime t = bdt::parse_delimited_time<bpt::ptime>(without_tz, 'T');
+
+    // if parsing succeeded, then return the time parsed. else fall back to
+    // printing an error.
+    if (!t.is_special()) {
+      return t;
+    }
   }
 
   std::ostringstream ostr;
   ostr << "Unable to parse string '" << s << "' as an ISO 8601 format date time.";
   throw std::runtime_error(ostr.str());
 }
-
