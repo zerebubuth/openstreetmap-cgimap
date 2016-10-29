b37872ad8f07280c4af6048b03b8ed5c0dd63655
Matt Amos <zerebubuth@gmail.com>
2016-10-29 15:56:17 +0100

    Refactored tests in apidb changesets file.

--- test/test_apidb_backend_changesets.cpp
+++ test/test_apidb_backend_changesets.cpp
@@ -33,7 +33,24 @@ void assert_equal(const T& a, const T&b, const std::string &message) {
   }
 }
 
-void test_negative_changeset_ids(boost::shared_ptr<data_selection> sel) {
+void test_negative_changeset_ids(test_database &tdb) {
+  tdb.run_sql(
+    "INSERT INTO users (id, email, pass_crypt, creation_time, display_name, data_public) "
+    "VALUES "
+    "  (-1, 'osmosis@osmosis.com', '', '2016-04-16T15:09:00Z', 'osmosis', false);"
+
+    "INSERT INTO changesets (id, user_id, created_at, closed_at) "
+    "VALUES "
+    "  (-1, -1, '2016-04-16T15:09:00Z', '2016-04-16T15:09:00Z'), "
+    "  (0, -1, '2016-04-16T15:09:00Z', '2016-04-16T15:09:00Z'); "
+
+    "INSERT INTO current_nodes (id, latitude, longitude, changeset_id, visible, \"timestamp\", tile, version) "
+    " VALUES "
+    "  (6, 90000000, 90000000,  0, true,  '2016-04-16T15:09:00Z', 3229120632, 1), "
+    "  (7, 90000000, 90000000, -1, true,  '2016-04-16T15:09:00Z', 3229120632, 1); "
+    );
+  boost::shared_ptr<data_selection> sel = tdb.get_data_selection();
+
   assert_equal<data_selection::visibility_t>(
     sel->check_node_visibility(6), data_selection::exists,
     "node 6 visibility");
@@ -67,7 +84,18 @@ void test_negative_changeset_ids(boost::shared_ptr<data_selection> sel) {
     f.m_nodes[1], "second node written");
 }
 
-void test_changeset(boost::shared_ptr<data_selection> sel) {
+void test_changeset(test_database &tdb) {
+  tdb.run_sql(
+    "INSERT INTO users (id, email, pass_crypt, creation_time, display_name, data_public) "
+    "VALUES "
+    "  (1, 'user_1@example.com', '', '2013-11-14T02:10:00Z', 'user_1', true); "
+
+    "INSERT INTO changesets (id, user_id, created_at, closed_at, num_changes) "
+    "VALUES "
+    "  (1, 1, '2013-11-14T02:10:00Z', '2013-11-14T03:10:00Z', 2);"
+    );
+  boost::shared_ptr<data_selection> sel = tdb.get_data_selection();
+
   assert_equal<bool>(sel->supports_changesets(), true,
                      "apidb should support changesets.");
 
@@ -103,7 +131,18 @@ void test_changeset(boost::shared_ptr<data_selection> sel) {
     "changesets");
 }
 
-void test_nonpublic_changeset(boost::shared_ptr<data_selection> sel) {
+void test_nonpublic_changeset(test_database &tdb) {
+  tdb.run_sql(
+    "INSERT INTO users (id, email, pass_crypt, creation_time, display_name, data_public) "
+    "VALUES "
+    "  (2, 'user_2@example.com', '', '2013-11-14T02:10:00Z', 'user_2', false); "
+
+    "INSERT INTO changesets (id, user_id, created_at, closed_at, num_changes) "
+    "VALUES "
+    "  (4, 2, '2013-11-14T02:10:00Z', '2013-11-14T03:10:00Z', 1);"
+    );
+  boost::shared_ptr<data_selection> sel = tdb.get_data_selection();
+
   assert_equal<bool>(sel->supports_changesets(), true,
                      "apidb should support changesets.");
 
@@ -139,7 +178,23 @@ void test_nonpublic_changeset(boost::shared_ptr<data_selection> sel) {
     "changesets");
 }
 
-void test_changeset_with_tags(boost::shared_ptr<data_selection> sel) {
+void test_changeset_with_tags(test_database &tdb) {
+  tdb.run_sql(
+    "INSERT INTO users (id, email, pass_crypt, creation_time, display_name, data_public) "
+    "VALUES "
+    "  (1, 'user_1@example.com', '', '2013-11-14T02:10:00Z', 'user_1', true); "
+
+    "INSERT INTO changesets (id, user_id, created_at, closed_at, num_changes) "
+    "VALUES "
+    "  (2, 1, '2013-11-14T02:10:00Z', '2013-11-14T03:10:00Z', 1);"
+
+    "INSERT INTO changeset_tags (changeset_id, k, v) "
+    "VALUES "
+    "  (2, 'test_key', 'test_value'), "
+    "  (2, 'test_key2', 'test_value2'); "
+    );
+  boost::shared_ptr<data_selection> sel = tdb.get_data_selection();
+
   assert_equal<bool>(sel->supports_changesets(), true,
                      "apidb should support changesets.");
 
@@ -178,8 +233,10 @@ void test_changeset_with_tags(boost::shared_ptr<data_selection> sel) {
     "changesets should be equal.");
 }
 
-void check_changeset_with_comments(boost::shared_ptr<data_selection> sel,
-                                   bool include_discussion) {
+void check_changeset_with_comments_impl(
+  boost::shared_ptr<data_selection> sel,
+  bool include_discussion) {
+
   assert_equal<bool>(sel->supports_changesets(), true,
                      "apidb should support changesets.");
 
@@ -229,20 +286,36 @@ void check_changeset_with_comments(boost::shared_ptr<data_selection> sel,
     "changesets should be equal.");
 }
 
-void test_changeset_with_comments_not_including_discussions(boost::shared_ptr<data_selection> sel) {
+void test_changeset_with_comments(test_database &tdb) {
+  tdb.run_sql(
+    "INSERT INTO users (id, email, pass_crypt, creation_time, display_name, data_public) "
+    "VALUES "
+    "  (1, 'user_1@example.com', '', '2013-11-14T02:10:00Z', 'user_1', true), "
+    "  (3, 'user_3@example.com', '', '2015-09-05T20:37:00Z', 'user_3', true); "
+
+    "INSERT INTO changesets (id, user_id, created_at, closed_at, num_changes) "
+    "VALUES "
+    "  (3, 1, '2013-11-14T02:10:00Z', '2013-11-14T03:10:00Z', 0); "
+
+    "INSERT INTO changeset_comments (id, changeset_id, author_id, body, created_at, visible) "
+    "VALUES "
+    "  (1, 3, 3, 'a nice comment!', '2015-09-05T20:37:01Z', true), "
+    "  (2, 3, 3, 'a nasty comment', '2015-09-05T20:37:10Z', false); "
+    );
+
   try {
-    check_changeset_with_comments(sel, false);
+    boost::shared_ptr<data_selection> sel = tdb.get_data_selection();
+    check_changeset_with_comments_impl(sel, false);
 
   } catch (const std::exception &e) {
     std::ostringstream ostr;
     ostr << e.what() << ", while include_discussion was false";
     throw std::runtime_error(ostr.str());
   }
-}
 
-void test_changeset_with_comments_including_discussions(boost::shared_ptr<data_selection> sel) {
   try {
-    check_changeset_with_comments(sel, true);
+    boost::shared_ptr<data_selection> sel = tdb.get_data_selection();
+    check_changeset_with_comments_impl(sel, true);
 
   } catch (const std::exception &e) {
     std::ostringstream ostr;
@@ -258,23 +331,20 @@ int main(int, char **) {
     test_database tdb;
     tdb.setup();
 
-    tdb.run(boost::function<void(boost::shared_ptr<data_selection>)>(
-        &test_negative_changeset_ids));
+    tdb.run(boost::function<void(test_database&)>(
+              &test_negative_changeset_ids));
 
-    tdb.run(boost::function<void(boost::shared_ptr<data_selection>)>(
+    tdb.run(boost::function<void(test_database&)>(
               &test_changeset));
 
-    tdb.run(boost::function<void(boost::shared_ptr<data_selection>)>(
+    tdb.run(boost::function<void(test_database&)>(
               &test_nonpublic_changeset));
 
-    tdb.run(boost::function<void(boost::shared_ptr<data_selection>)>(
+    tdb.run(boost::function<void(test_database&)>(
               &test_changeset_with_tags));
 
-    tdb.run(boost::function<void(boost::shared_ptr<data_selection>)>(
-              &test_changeset_with_comments_not_including_discussions));
-
-    tdb.run(boost::function<void(boost::shared_ptr<data_selection>)>(
-              &test_changeset_with_comments_including_discussions));
+    tdb.run(boost::function<void(test_database&)>(
+              &test_changeset_with_comments));
 
   } catch (const test_database::setup_error &e) {
     std::cout << "Unable to set up test database: " << e.what() << std::endl;
