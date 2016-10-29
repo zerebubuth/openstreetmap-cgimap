9b72fab2838f943498c68224a9cf6beae3279708
Matt Amos <zerebubuth@gmail.com>
2016-10-29 17:01:45 +0100

    Refactor historic tests.

--- test/test_apidb_backend_historic.cpp
+++ test/test_apidb_backend_historic.cpp
@@ -33,7 +33,29 @@ void assert_equal(const T& a, const T&b, const std::string &message) {
   }
 }
 
-void test_historic_elements(boost::shared_ptr<data_selection> sel) {
+void test_historic_elements(test_database &tdb) {
+  tdb.run_sql(
+    "INSERT INTO users (id, email, pass_crypt, creation_time, display_name, data_public) "
+    "VALUES "
+    "  (1, 'user_1@example.com', '', '2013-11-14T02:10:00Z', 'user_1', true); "
+
+    "INSERT INTO changesets (id, user_id, created_at, closed_at) "
+    "VALUES "
+    "  (2, 1, '2013-11-14T02:10:00Z', '2013-11-14T03:10:00Z'); "
+
+    "INSERT INTO current_nodes (id, latitude, longitude, changeset_id, "
+    "                           visible, \"timestamp\", tile, version) "
+    "VALUES "
+    "  (3, 0, 0, 2, false, '2015-03-02T18:27:00Z', 3221225472, 2); "
+
+    "INSERT INTO nodes (node_id, latitude, longitude, changeset_id, visible, "
+    "                   \"timestamp\", tile, version, redaction_id) "
+    "VALUES "
+    "  (3, 0, 0, 2, true,  '2015-03-02T18:27:00Z', 3221225472, 1, NULL), "
+    "  (3, 0, 0, 2, false, '2015-03-02T18:27:00Z', 3221225472, 2, NULL); "
+    "");
+  boost::shared_ptr<data_selection> sel = tdb.get_data_selection();
+
   assert_equal<bool>(
     sel->supports_historical_versions(), true,
     "data selection supports historical versions");
@@ -66,7 +88,29 @@ void test_historic_elements(boost::shared_ptr<data_selection> sel) {
     f.m_nodes[1], "second node written");
 }
 
-void test_historic_dup(boost::shared_ptr<data_selection> sel) {
+void test_historic_dup(test_database &tdb) {
+  tdb.run_sql(
+    "INSERT INTO users (id, email, pass_crypt, creation_time, display_name, data_public) "
+    "VALUES "
+    "  (1, 'user_1@example.com', '', '2013-11-14T02:10:00Z', 'user_1', true); "
+
+    "INSERT INTO changesets (id, user_id, created_at, closed_at) "
+    "VALUES "
+    "  (2, 1, '2013-11-14T02:10:00Z', '2013-11-14T03:10:00Z'); "
+
+    "INSERT INTO current_nodes (id, latitude, longitude, changeset_id, "
+    "                           visible, \"timestamp\", tile, version) "
+    "VALUES "
+    "  (3, 0, 0, 2, false, '2015-03-02T18:27:00Z', 3221225472, 2); "
+
+    "INSERT INTO nodes (node_id, latitude, longitude, changeset_id, visible, "
+    "                   \"timestamp\", tile, version, redaction_id) "
+    "VALUES "
+    "  (3, 0, 0, 2, true,  '2015-03-02T18:27:00Z', 3221225472, 1, NULL), "
+    "  (3, 0, 0, 2, false, '2015-03-02T18:27:00Z', 3221225472, 2, NULL); "
+    "");
+  boost::shared_ptr<data_selection> sel = tdb.get_data_selection();
+
   assert_equal<bool>(
     sel->supports_historical_versions(), true,
     "data selection supports historical versions");
@@ -96,7 +140,43 @@ void test_historic_dup(boost::shared_ptr<data_selection> sel) {
     f.m_nodes[0], "node written");
 }
 
-void test_historic_dup_way(boost::shared_ptr<data_selection> sel) {
+void test_historic_dup_way(test_database &tdb) {
+  tdb.run_sql(
+    "INSERT INTO users (id, email, pass_crypt, creation_time, display_name, data_public) "
+    "VALUES "
+    "  (1, 'user_1@example.com', '', '2013-11-14T02:10:00Z', 'user_1', true); "
+
+    "INSERT INTO changesets (id, user_id, created_at, closed_at) "
+    "VALUES "
+    "  (3, 1, '2013-11-14T02:10:00Z', '2013-11-14T03:10:00Z'); "
+
+    "INSERT INTO current_nodes (id, latitude, longitude, changeset_id, "
+    "                           visible, \"timestamp\", tile, version) "
+    "VALUES "
+    "  (3, 0, 0, 3, false, '2015-03-02T18:27:00Z', 3221225472, 2); "
+
+    "INSERT INTO current_ways (id, changeset_id, \"timestamp\", visible, version) "
+    "VALUES "
+    "  (1, 3, '2016-09-06T19:55:00Z', true, 2); "
+
+    "INSERT INTO current_way_nodes (way_id, node_id, sequence_id) "
+    "VALUES "
+    "  (1, 3, 1); "
+
+    "INSERT INTO ways (way_id, changeset_id, \"timestamp\", visible, "
+    "                  version, redaction_id) "
+    "VALUES "
+    "  (1, 3, '2016-09-06T19:55:00Z', true, 2, NULL), "
+    "  (1, 3, '2016-09-06T19:54:00Z', true, 1, NULL); "
+
+    "INSERT INTO way_nodes (way_id, version, node_id, sequence_id) "
+    "VALUES "
+    "  (1, 2, 3, 1), "
+    "  (1, 1, 3, 1), "
+    "  (1, 1, 2, 2); "
+    "");
+  boost::shared_ptr<data_selection> sel = tdb.get_data_selection();
+
   assert_equal<bool>(
     sel->supports_historical_versions(), true,
     "data selection supports historical versions");
@@ -129,7 +209,45 @@ void test_historic_dup_way(boost::shared_ptr<data_selection> sel) {
     f.m_ways[0], "way written");
 }
 
-void test_historic_dup_relation(boost::shared_ptr<data_selection> sel) {
+void test_historic_dup_relation(test_database &tdb) {
+  tdb.run_sql(
+    "INSERT INTO users (id, email, pass_crypt, creation_time, display_name, data_public) "
+    "VALUES "
+    "  (1, 'user_1@example.com', '', '2013-11-14T02:10:00Z', 'user_1', true); "
+
+    "INSERT INTO changesets (id, user_id, created_at, closed_at) "
+    "VALUES "
+    "  (3, 1, '2013-11-14T02:10:00Z', '2013-11-14T03:10:00Z'); "
+
+    "INSERT INTO current_nodes (id, latitude, longitude, changeset_id, "
+    "                           visible, \"timestamp\", tile, version) "
+    "VALUES "
+    "  (3, 0, 0, 3, false, '2015-03-02T18:27:00Z', 3221225472, 2); "
+
+    "INSERT INTO current_relations (id, changeset_id, \"timestamp\", "
+    "                               visible, version) "
+    "VALUES "
+    "  (1, 3, '2016-09-19T18:49:00Z', true, 2); "
+
+    "INSERT INTO current_relation_members ("
+        "relation_id, member_type, member_id, member_role, sequence_id) "
+    "VALUES "
+    "  (1, 'Node', 3, 'foo', 1); "
+
+    "INSERT INTO relations (relation_id, changeset_id, \"timestamp\", "
+    "                       visible, version, redaction_id) "
+    "VALUES "
+    "  (1, 3, '2016-09-19T18:49:00Z', true, 2, NULL), "
+    "  (1, 3, '2016-09-19T18:48:00Z', true, 1, NULL); "
+
+    "INSERT INTO relation_members (relation_id, member_type, member_id, "
+    "                              member_role, sequence_id, version) "
+    "VALUES "
+    "  (1, 'Node', 3, 'foo', 1, 2), "
+    "  (1, 'Node', 3, 'bar', 1, 1); "
+    "");
+  boost::shared_ptr<data_selection> sel = tdb.get_data_selection();
+
   assert_equal<bool>(
     sel->supports_historical_versions(), true,
     "data selection supports historical versions");
@@ -169,16 +287,16 @@ int main(int, char **) {
     test_database tdb;
     tdb.setup();
 
-    tdb.run(boost::function<void(boost::shared_ptr<data_selection>)>(
+    tdb.run(boost::function<void(test_database&)>(
         &test_historic_elements));
 
-    tdb.run(boost::function<void(boost::shared_ptr<data_selection>)>(
+    tdb.run(boost::function<void(test_database&)>(
         &test_historic_dup));
 
-    tdb.run(boost::function<void(boost::shared_ptr<data_selection>)>(
+    tdb.run(boost::function<void(test_database&)>(
         &test_historic_dup_way));
 
-    tdb.run(boost::function<void(boost::shared_ptr<data_selection>)>(
+    tdb.run(boost::function<void(test_database&)>(
         &test_historic_dup_relation));
 
   } catch (const test_database::setup_error &e) {
