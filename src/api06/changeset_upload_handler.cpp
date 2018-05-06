576e0fa1c0f29b039c5b2ed8d8d5b0b6327495ba
mmd-osm <mmd.osm@gmail.com>
2018-05-06 21:19:49 +0200

    Moved temporary table creation until we have a better place for it

--- src/api06/changeset_upload_handler.cpp
+++ src/api06/changeset_upload_handler.cpp
@@ -41,6 +41,51 @@ changeset_upload_responder::changeset_upload_responder(
 
   Transaction_Manager m { "dbname=openstreetmap" };
 
+  // ***************************************
+
+  // TODO: Creating temporary tables needs to be moved to a proper location, once it is
+  // decided where the db connection for changeset uploads is managed!
+
+  // temporary tables for changeset upload
+  m.exec(R"(CREATE TEMPORARY TABLE tmp_create_nodes 
+              (
+                id bigint NOT NULL DEFAULT nextval('current_nodes_id_seq'::regclass),  
+                latitude integer NOT NULL,
+                longitude integer NOT NULL,
+                changeset_id bigint NOT NULL,
+                visible boolean NOT NULL DEFAULT true,
+                "timestamp" timestamp without time zone NOT NULL DEFAULT (now() at time zone 'utc'),
+                tile bigint NOT NULL,
+                version bigint NOT NULL DEFAULT 1,
+                old_id bigint NOT NULL UNIQUE,
+                PRIMARY KEY (id)) 
+              )");
+
+  m.exec(R"(CREATE TEMPORARY TABLE tmp_create_ways 
+              (
+                id bigint NOT NULL DEFAULT nextval('current_ways_id_seq'::regclass),  
+                changeset_id bigint NOT NULL,
+                visible boolean NOT NULL DEFAULT true,
+                "timestamp" timestamp without time zone NOT NULL DEFAULT (now() at time zone 'utc'),
+                version bigint NOT NULL DEFAULT 1,
+                old_id bigint NOT NULL UNIQUE,
+                PRIMARY KEY (id)) 
+              )");
+
+  m.exec(R"(CREATE TEMPORARY TABLE tmp_create_relations 
+              (
+                id bigint NOT NULL DEFAULT nextval('current_relations_id_seq'::regclass),  
+                changeset_id bigint NOT NULL,
+                visible boolean NOT NULL DEFAULT true,
+                "timestamp" timestamp without time zone NOT NULL DEFAULT (now() at time zone 'utc'),
+                version bigint NOT NULL DEFAULT 1,
+                old_id bigint NOT NULL UNIQUE,
+                PRIMARY KEY (id)) 
+              )");
+
+  // ***************************************
+
+
   change_tracking = std::make_shared<OSMChange_Tracking>();
 
   // TODO: we're still in api06 code, don't use ApiDB_*_Updater here!!
