6588f1b310fa0dfcc238948a4ce14f78df408e22
mmd-osm <mmd.osm@gmail.com>
2020-06-18 21:16:26 +0200

    Expose global settings via boost program options

--- include/cgimap/options.hpp
+++ include/cgimap/options.hpp
@@ -2,57 +2,133 @@
 #define OPTIONS_HPP
 
 #include <memory>
+#include <regex>
+#include <boost/program_options.hpp>
 
+namespace po = boost::program_options;
 
 class global_settings_base {
 
 public:
   virtual ~global_settings_base();
 
-  virtual long get_payload_max_size() = 0;
-  virtual int get_map_max_nodes() = 0;
-  virtual double get_map_area_max() = 0;
-  virtual std::string get_changeset_timeout_open_max() = 0;
-  virtual std::string get_changeset_timeout_idle() = 0;
-  virtual int get_changeset_max_elements() = 0;
-  virtual int get_way_max_nodes() = 0;
-  virtual long get_scale() = 0;
+  virtual long get_payload_max_size() const = 0;
+  virtual int get_map_max_nodes() const = 0;
+  virtual double get_map_area_max() const = 0;
+  virtual std::string get_changeset_timeout_open_max() const = 0;
+  virtual std::string get_changeset_timeout_idle() const = 0;
+  virtual int get_changeset_max_elements() const = 0;
+  virtual int get_way_max_nodes() const = 0;
+  virtual long get_scale() const = 0;
 };
 
 class global_settings_default : public global_settings_base {
 
 public:
-  long get_payload_max_size() override {
+  long get_payload_max_size() const override {
     return 50000000L;
   }
 
-  int get_map_max_nodes() override {
+  int get_map_max_nodes() const override {
     return 50000;
   }
 
-  double get_map_area_max() override {
+  double get_map_area_max() const override {
      return 0.25;
   }
 
-  std::string get_changeset_timeout_open_max() override {
+  std::string get_changeset_timeout_open_max() const override {
      return "1 day";
   }
 
-  std::string get_changeset_timeout_idle() override {
+  std::string get_changeset_timeout_idle() const override {
      return "1 hour";
   }
 
-  int get_changeset_max_elements() override {
+  int get_changeset_max_elements() const override {
      return 10000;
   }
 
-  int get_way_max_nodes() override {
+  int get_way_max_nodes() const override {
      return 2000;
   }
 
-  long get_scale() override {
+  long get_scale() const override {
      return 10000000L;
   }
+};
+
+class global_settings_via_options : public global_settings_base {
+
+public:
+  global_settings_via_options() = delete;
+
+  global_settings_via_options(const po::variables_map & options) {
+
+    init_fallback_values(global_settings_default{}); // use default values as fallback
+    set_new_options(options);
+  }
+
+  global_settings_via_options(const po::variables_map & options,
+			      const global_settings_base & fallback) {
+
+    init_fallback_values(fallback);
+    set_new_options(options);
+  }
+
+  long get_payload_max_size() const override {
+    return m_payload_max_size;
+  }
+
+  int get_map_max_nodes() const override {
+    return m_map_max_nodes;
+  }
+
+  double get_map_area_max() const override {
+     return m_map_area_max;
+  }
+
+  std::string get_changeset_timeout_open_max() const override {
+     return m_changeset_timeout_open_max;
+  }
+
+  std::string get_changeset_timeout_idle() const override {
+     return m_changeset_timeout_idle;
+  }
+
+  int get_changeset_max_elements() const override {
+     return m_changeset_max_elements;
+  }
+
+  int get_way_max_nodes() const override {
+     return m_way_max_nodes;
+  }
+
+  long get_scale() const override {
+     return m_scale;
+  }
+
+private:
+  void init_fallback_values(const global_settings_base & def);
+  void set_new_options(const po::variables_map & options);
+  void set_payload_max_size(const po::variables_map & options);
+  void set_map_max_nodes(const po::variables_map & options);
+  void set_map_area_max(const po::variables_map & options);
+  void set_changeset_timeout_open_max(const po::variables_map & options);
+  void set_changeset_timeout_idle(const po::variables_map & options);
+  void set_changeset_max_elements(const po::variables_map & options);
+  void set_way_max_nodes(const po::variables_map & options);
+  void set_scale(const po::variables_map & options);
+  bool validate_timeout(const std::string &timeout) const;
+
+  long m_payload_max_size;
+  int  m_map_max_nodes;
+  double m_map_area_max;
+  std::string m_changeset_timeout_open_max;
+  std::string m_changeset_timeout_idle;
+  int m_changeset_max_elements;
+  int m_way_max_nodes;
+  long m_scale;
 };
 
 class global_settings final {
--- src/options.cpp
+++ src/options.cpp
@@ -5,3 +5,104 @@
 global_settings_base::~global_settings_base() = default;
 
 std::unique_ptr<global_settings_base> global_settings::settings = std::unique_ptr<global_settings_base>(new global_settings_default());
+
+
+void global_settings_via_options::init_fallback_values(const global_settings_base & def) {
+
+  m_payload_max_size = def.get_payload_max_size();
+  m_map_max_nodes = def.get_map_max_nodes();
+  m_map_area_max = def.get_map_area_max();
+  m_changeset_timeout_open_max = def.get_changeset_timeout_open_max();
+  m_changeset_timeout_idle = def.get_changeset_timeout_idle();
+  m_changeset_max_elements = def.get_changeset_max_elements();
+  m_way_max_nodes = def.get_way_max_nodes();
+  m_scale = def.get_scale();
+}
+
+void global_settings_via_options::set_new_options(const po::variables_map & options) {
+
+  set_payload_max_size(options);
+  set_map_max_nodes(options);
+  set_map_area_max(options);
+  set_changeset_timeout_open_max(options);
+  set_changeset_timeout_idle(options);
+  set_changeset_max_elements(options);
+  set_way_max_nodes(options);
+  set_scale(options);
+}
+
+void global_settings_via_options::set_payload_max_size(const po::variables_map & options)  {
+  if (options.count("max_payload_size")) {
+    m_payload_max_size = options["max_payload_size"].as<long>();
+    if (m_payload_max_size <= 0)
+      throw std::invalid_argument("max_payload_size must be a positive number");
+  }
+}
+
+void global_settings_via_options::set_map_max_nodes(const po::variables_map & options)  {
+  if (options.count("map_nodes_max")) {
+    m_map_max_nodes = options["map_nodes_max"].as<int>();
+    if (m_map_max_nodes <= 0)
+	throw std::invalid_argument("map_nodes_max must be a positive number");
+  }
+}
+
+void global_settings_via_options::set_map_area_max(const po::variables_map & options) {
+  if (options.count("map_area_max")) {
+   m_map_area_max = options["map_area_max"].as<double>();
+   if (m_map_area_max <= 0)
+     throw std::invalid_argument("map_area_max must be a positive number");
+  }
+}
+
+void global_settings_via_options::set_changeset_timeout_open_max(const po::variables_map & options) {
+  if (options.count("changeset_timeout_open_max")) {
+    m_changeset_timeout_open_max = options["changeset_timeout_open_max"].as<std::string>();
+    if (!validate_timeout(m_changeset_timeout_open_max))
+      throw std::invalid_argument("Invalid changeset max open timeout value");
+  }
+}
+
+void global_settings_via_options::set_changeset_timeout_idle(const po::variables_map & options)  {
+  if (options.count("changeset_timeout_idle")) {
+    m_changeset_timeout_idle = options["changeset_timeout_idle"].as<std::string>();
+    if (!validate_timeout(m_changeset_timeout_idle)) {
+      throw std::invalid_argument("Invalid changeset idle timeout value");
+    }
+  }
+}
+
+void global_settings_via_options::set_changeset_max_elements(const po::variables_map & options)  {
+  if (options.count("changeset_max_elements")) {
+    m_changeset_max_elements = options["changeset_max_elements"].as<int>();
+    if (m_changeset_max_elements <= 0)
+      throw std::invalid_argument("changeset_max_elements must be a positive number");
+  }
+}
+
+void global_settings_via_options::set_way_max_nodes(const po::variables_map & options)  {
+  if (options.count("way_max_nodes")) {
+    m_way_max_nodes = options["way_max_nodes"].as<int>();
+    if (m_way_max_nodes <= 0)
+      throw std::invalid_argument("way_max_nodes must be a positive number");
+  }
+}
+
+void global_settings_via_options::set_scale(const po::variables_map & options) {
+  if (options.count("scale")) {
+    m_scale = options["scale"].as<long>();
+    if (m_scale <= 0)
+      throw std::invalid_argument("scale must be a positive number");
+  }
+}
+
+bool global_settings_via_options::validate_timeout(const std::string &timeout) const {
+  std::smatch sm;
+  try {
+    std::regex r("[0-9]+ (day|hour|minute|second)s?");
+    if (std::regex_match(timeout, sm, r)) {
+      return true;
+    }
+  } catch (std::regex_error &) {}
+  return false;
+}
