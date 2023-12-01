8f85d02473b45ef0c4a06e7a6bb6025e68c2af94
mmd-osm <mmd.osm@gmail.com>
2023-12-01 22:19:17 +0100

    Migrate test_apidb_backend_nodes to Catch2

--- test/test_apidb_backend_nodes.cpp
+++ test/test_apidb_backend_nodes.cpp
@@ -28,229 +28,209 @@
 #include "test_database.hpp"
 #include "test_request.hpp"
 
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
 namespace {
 
-template <typename T>
-void assert_equal(const T& a, const T&b, const std::string &message) {
-  if (a != b) {
-    throw std::runtime_error(fmt::format("Expecting {} to be equal, but {} != {}", message, a, b));
-  }
-}
+class DatabaseTestsFixture
+{
+protected:
+  DatabaseTestsFixture() = default;
+  static test_database tdb;
+};
 
-void test_single_nodes(test_database &tdb) {
-  tdb.run_sql(
-    "INSERT INTO users (id, email, pass_crypt, creation_time, display_name, data_public) "
-    "VALUES "
-    "  (1, 'user_1@example.com', '', '2013-11-14T02:10:00Z', 'user_1', true), "
-    "  (2, 'user_2@example.com', '', '2013-11-14T02:10:00Z', 'user_2', false); "
+test_database DatabaseTestsFixture::tdb{};
 
-    "INSERT INTO changesets (id, user_id, created_at, closed_at) "
-    "VALUES "
-    "  (1, 1, '2013-11-14T02:10:00Z', '2013-11-14T03:10:00Z'), "
-    "  (2, 1, '2013-11-14T02:10:00Z', '2013-11-14T03:10:00Z'), "
-    "  (4, 2, '2013-11-14T02:10:00Z', '2013-11-14T03:10:00Z');"
+struct CGImapListener : Catch::TestEventListenerBase, DatabaseTestsFixture {
 
-    "INSERT INTO current_nodes (id, latitude, longitude, changeset_id, visible, \"timestamp\", tile, version) "
-    " VALUES "
-    "  (1,       0,       0, 1, true,  '2013-11-14T02:10:00Z', 3221225472, 1), "
-    "  (2, 1000000, 1000000, 1, true,  '2013-11-14T02:10:01Z', 3221227032, 1), "
-    "  (3,       0,       0, 2, false, '2015-03-02T18:27:00Z', 3221225472, 2), "
-    "  (4,       0,       0, 4, true,  '2015-03-02T19:25:00Z', 3221225472, 1);"
-    );
-  auto sel = tdb.get_data_selection();
+    using TestEventListenerBase::TestEventListenerBase; // inherit constructor
 
-  if (sel->check_node_visibility(1) != data_selection::exists) {
-    throw std::runtime_error("Node 1 should be visible, but isn't");
-  }
-  if (sel->check_node_visibility(2) != data_selection::exists) {
-    throw std::runtime_error("Node 2 should be visible, but isn't");
-  }
+    void testRunStarting( Catch::TestRunInfo const& testRunInfo ) override {
+      // load database schema when starting up tests
+      tdb.setup();
+    }
 
-  std::vector<osm_nwr_id_t> ids{1,2,3,4};
+    void testCaseStarting( Catch::TestCaseInfo const& testInfo ) override {
+      tdb.testcase_starting();
+    }
 
-  if (sel->select_nodes(ids) != 4) {
-    throw std::runtime_error("Selecting 4 nodes failed");
-  }
-  if (sel->select_nodes(ids) != 0) {
-    throw std::runtime_error("Re-selecting 4 nodes failed");
-  }
+    void testCaseEnded( Catch::TestCaseStats const& testCaseStats ) override {
+      tdb.testcase_ended();
+    }
+};
 
-  assert_equal<data_selection::visibility_t>(
-    sel->check_node_visibility(1), data_selection::exists,
-    "node 1 visibility");
-  assert_equal<data_selection::visibility_t>(
-    sel->check_node_visibility(2), data_selection::exists,
-    "node 2 visibility");
-  assert_equal<data_selection::visibility_t>(
-    sel->check_node_visibility(3), data_selection::deleted,
-    "node 3 visibility");
-  assert_equal<data_selection::visibility_t>(
-    sel->check_node_visibility(4), data_selection::exists,
-    "node 4 visibility");
-  assert_equal<data_selection::visibility_t>(
-    sel->check_node_visibility(5), data_selection::non_exist,
-    "node 5 visibility");
-
-  test_formatter f;
-  sel->write_nodes(f);
-  assert_equal<size_t>(f.m_nodes.size(), 4, "number of nodes written");
-  assert_equal<test_formatter::node_t>(
-    test_formatter::node_t(
-      element_info(1, 1, 1, "2013-11-14T02:10:00Z", 1, std::string("user_1"), true),
-      0.0, 0.0,
-      tags_t()
-      ),
-    f.m_nodes[0], "first node written");
-  assert_equal<test_formatter::node_t>(
-    test_formatter::node_t(
-      element_info(2, 1, 1, "2013-11-14T02:10:01Z", 1, std::string("user_1"), true),
-      0.1, 0.1,
-      tags_t()
-      ),
-    f.m_nodes[1], "second node written");
-  assert_equal<test_formatter::node_t>(
-    test_formatter::node_t(
-      element_info(3, 2, 2, "2015-03-02T18:27:00Z", 1, std::string("user_1"), false),
-      0.0, 0.0,
-      tags_t()
-      ),
-    f.m_nodes[2], "third node written");
-  assert_equal<test_formatter::node_t>(
-    test_formatter::node_t(
-      element_info(4, 1, 4, "2015-03-02T19:25:00Z", {}, {}, true),
-      0.0, 0.0,
-      tags_t()
-      ),
-    f.m_nodes[3], "fourth (anonymous) node written");
-}
-
-void test_dup_nodes(test_database &tdb) {
-  tdb.run_sql(
-    "INSERT INTO users (id, email, pass_crypt, creation_time, display_name, data_public) "
-    "VALUES "
-    "  (1, 'user_1@example.com', '', '2013-11-14T02:10:00Z', 'user_1', true); "
-
-    "INSERT INTO changesets (id, user_id, created_at, closed_at) "
-    "VALUES "
-    "  (1, 1, '2013-11-14T02:10:00Z', '2013-11-14T03:10:00Z');"
-
-    "INSERT INTO current_nodes (id, latitude, longitude, changeset_id, visible, \"timestamp\", tile, version) "
-    " VALUES "
-    "  (1,       0,       0, 1, true,  '2013-11-14T02:10:00Z', 3221225472, 1);"
-    );
-
-  auto sel = tdb.get_data_selection();
-
-  if (sel->check_node_visibility(1) != data_selection::exists) {
-    throw std::runtime_error("Node 1 should be visible, but isn't");
-  }
-
-  std::vector<osm_nwr_id_t> ids{1,1,1};
-
-  if (sel->select_nodes(ids) != 1) {
-    throw std::runtime_error("Selecting 3 duplicates of 1 node failed");
-  }
-  if (sel->select_nodes(ids) != 0) {
-    throw std::runtime_error("Re-selecting the same node failed");
-  }
-
-  assert_equal<data_selection::visibility_t>(
-    sel->check_node_visibility(1), data_selection::exists,
-    "node 1 visibility");
-
-  test_formatter f;
-  sel->write_nodes(f);
-  assert_equal<size_t>(f.m_nodes.size(), 1, "number of nodes written");
-  assert_equal<test_formatter::node_t>(
-    test_formatter::node_t(
-      element_info(1, 1, 1, "2013-11-14T02:10:00Z", 1, std::string("user_1"), true),
-      0.0, 0.0,
-      tags_t()
-      ),
-    f.m_nodes[0], "first node written");
-}
-
-void test_psql_array_to_vector() {
-  std::string test = "{NULL}";
-  std::vector<std::string> actual_values;
-  std::vector<std::string> values = psql_array_to_vector(test);
-
-  if (values != actual_values) {
-    throw std::runtime_error("Psql array parse failed for " + test);
-  }
-
-  test = "{1,2}";
-  values = psql_array_to_vector(test);
-  actual_values.clear();
-  actual_values.push_back("1");
-  actual_values.push_back("2");
-  if (values != actual_values) {
-    throw std::runtime_error("Psql array parse failed for " + test);
-  }
-
-  test = "{\"TEST\",TEST123}";
-  values = psql_array_to_vector(test);
-  actual_values.clear();
-  actual_values.push_back("TEST");
-  actual_values.push_back("TEST123");
-  if (values != actual_values) {
-    throw std::runtime_error("Psql array parse failed for " + test);
-  }
-
-  test = R"({"},\"",",{}}\\"})";
-  values = psql_array_to_vector(test);
-  actual_values.clear();
-  actual_values.push_back("},\"");
-  actual_values.push_back(",{}}\\");
-  if (values != actual_values) {
-    throw std::runtime_error("Psql array parse failed for " + test + " " +values[0] + " " +values[1]);
-  }
-
-  // test with semicolon in key
-  test = R"({use_sidepath,secondary,3,1,yes,50,"Rijksweg Noord",asphalt,left|through;right})";
-  values = psql_array_to_vector(test);
-  actual_values.clear();
-  actual_values.push_back("use_sidepath");
-  actual_values.push_back("secondary");
-  actual_values.push_back("3");
-  actual_values.push_back("1");
-  actual_values.push_back("yes");
-  actual_values.push_back("50");
-  actual_values.push_back("Rijksweg Noord");
-  actual_values.push_back("asphalt");
-  actual_values.push_back("left|through;right");
-  if (values != actual_values) {
-    throw std::runtime_error("Psql array parse failed for " + test + " " +values[0] + " " +values[1]);
-  }
-}
+CATCH_REGISTER_LISTENER( CGImapListener )
 
 } // anonymous namespace
 
-int main(int, char **) {
-  try {
-    test_database tdb;
-    tdb.setup();
 
-    test_psql_array_to_vector();
+TEST_CASE_METHOD( DatabaseTestsFixture, "test_single_nodes", "[nodes][db]" ) {
 
-    tdb.run(std::function<void(test_database&)>(
-        &test_single_nodes));
+  auto sel = tdb.get_data_selection();
 
-    tdb.run(std::function<void(test_database&)>(
-        &test_dup_nodes));
+  SECTION("Initialize test data") {
 
-  } catch (const test_database::setup_error &e) {
-    std::cout << "Unable to set up test database: " << e.what() << std::endl;
-    return 77;
+    tdb.run_sql(
+      "INSERT INTO users (id, email, pass_crypt, creation_time, display_name, data_public) "
+      "VALUES "
+      "  (1, 'user_1@example.com', '', '2013-11-14T02:10:00Z', 'user_1', true), "
+      "  (2, 'user_2@example.com', '', '2013-11-14T02:10:00Z', 'user_2', false); "
 
-  } catch (const std::exception &e) {
-    std::cout << "Error: " << e.what() << std::endl;
-    return 1;
+      "INSERT INTO changesets (id, user_id, created_at, closed_at) "
+      "VALUES "
+      "  (1, 1, '2013-11-14T02:10:00Z', '2013-11-14T03:10:00Z'), "
+      "  (2, 1, '2013-11-14T02:10:00Z', '2013-11-14T03:10:00Z'), "
+      "  (4, 2, '2013-11-14T02:10:00Z', '2013-11-14T03:10:00Z');"
 
-  } catch (...) {
-    std::cout << "Unknown exception type." << std::endl;
-    return 99;
+      "INSERT INTO current_nodes (id, latitude, longitude, changeset_id, visible, \"timestamp\", tile, version) "
+      " VALUES "
+      "  (1,       0,       0, 1, true,  '2013-11-14T02:10:00Z', 3221225472, 1), "
+      "  (2, 1000000, 1000000, 1, true,  '2013-11-14T02:10:01Z', 3221227032, 1), "
+      "  (3,       0,       0, 2, false, '2015-03-02T18:27:00Z', 3221225472, 2), "
+      "  (4,       0,       0, 4, true,  '2015-03-02T19:25:00Z', 3221225472, 1);"
+      );
   }
 
-  return 0;
+  SECTION("Perform single node checks") {
+
+    std::vector<osm_nwr_id_t> ids{1,2,3,4};
+
+    REQUIRE (sel->select_nodes(ids) == 4); // 4 ids read from database
+    REQUIRE (sel->select_nodes(ids) == 0); // Re-selecting 4 nodes failed, values should be still in buffer
+
+    REQUIRE (sel->check_node_visibility(1) == data_selection::exists);
+    REQUIRE (sel->check_node_visibility(2) == data_selection::exists);
+    REQUIRE (sel->check_node_visibility(3) == data_selection::deleted);
+    REQUIRE (sel->check_node_visibility(4) == data_selection::exists);
+    REQUIRE (sel->check_node_visibility(5) == data_selection::non_exist);
+
+    test_formatter f;
+    sel->write_nodes(f);
+
+    REQUIRE (f.m_nodes.size() == 4);
+
+    REQUIRE(
+      test_formatter::node_t(
+        element_info(1, 1, 1, "2013-11-14T02:10:00Z", 1, std::string("user_1"), true),
+        0.0, 0.0,
+        tags_t()
+        ) == f.m_nodes[0]);
+
+    REQUIRE(
+      test_formatter::node_t(
+        element_info(2, 1, 1, "2013-11-14T02:10:01Z", 1, std::string("user_1"), true),
+        0.1, 0.1,
+        tags_t()
+        ) == f.m_nodes[1]);
+
+    REQUIRE(
+      test_formatter::node_t(
+        element_info(3, 2, 2, "2015-03-02T18:27:00Z", 1, std::string("user_1"), false),
+        0.0, 0.0,
+        tags_t()
+        ) == f.m_nodes[2]);
+
+    REQUIRE(
+      test_formatter::node_t(
+        element_info(4, 1, 4, "2015-03-02T19:25:00Z", {}, {}, true),
+        0.0, 0.0,
+        tags_t()
+        ) == f.m_nodes[3]);
+  }
+}
+
+TEST_CASE_METHOD( DatabaseTestsFixture, "test_dup_nodes", "[nodes][db]" ) {
+
+  auto sel = tdb.get_data_selection();
+
+  SECTION("Initialize test data") {
+    tdb.run_sql(
+      "INSERT INTO users (id, email, pass_crypt, creation_time, display_name, data_public) "
+      "VALUES "
+      "  (1, 'user_1@example.com', '', '2013-11-14T02:10:00Z', 'user_1', true); "
+
+      "INSERT INTO changesets (id, user_id, created_at, closed_at) "
+      "VALUES "
+      "  (1, 1, '2013-11-14T02:10:00Z', '2013-11-14T03:10:00Z');"
+
+      "INSERT INTO current_nodes (id, latitude, longitude, changeset_id, visible, \"timestamp\", tile, version) "
+      " VALUES "
+      "  (1,       0,       0, 1, true,  '2013-11-14T02:10:00Z', 3221225472, 1);"
+      );
+  }
+
+  SECTION("Perform duplicate node checks") {
+
+    REQUIRE (sel->check_node_visibility(1) == data_selection::exists);
+
+    std::vector<osm_nwr_id_t> ids{1,1,1};
+
+    REQUIRE (sel->select_nodes(ids) == 1);
+    REQUIRE (sel->select_nodes(ids) == 0); // Re-selecting the same node failed
+
+    REQUIRE (sel->check_node_visibility(1) == data_selection::exists);
+
+    test_formatter f;
+    sel->write_nodes(f);
+
+    REQUIRE (f.m_nodes.size() == 1);
+
+    REQUIRE(
+      test_formatter::node_t(
+        element_info(1, 1, 1, "2013-11-14T02:10:00Z", 1, std::string("user_1"), true),
+        0.0, 0.0,
+        tags_t()
+        ) == f.m_nodes[0]);
+  }
+}
+
+TEST_CASE("test_psql_array_to_vector", "[nodb]") {
+
+  std::string test;
+  std::vector<std::string> actual_values;
+  std::vector<std::string> values;
+
+  SECTION("NULL") {
+    test = "{NULL}";
+    values = psql_array_to_vector(test);
+    REQUIRE (values == actual_values);
+  }
+
+  SECTION("Two values") {
+    test = "{1,2}";
+    values = psql_array_to_vector(test);
+    actual_values = {"1", "2"};
+    REQUIRE (values == actual_values);
+  }
+
+  SECTION("Two strings") {
+    test = "{\"TEST\",TEST123}";
+    values = psql_array_to_vector(test);
+    actual_values = { "TEST", "TEST123" };
+    REQUIRE (values == actual_values);
+  }
+
+  SECTION("Complex pattern") {
+    test = R"({"},\"",",{}}\\"})";
+    values = psql_array_to_vector(test);
+    actual_values.push_back("},\"");
+    actual_values.push_back(",{}}\\");
+    REQUIRE (values == actual_values);
+  }
+
+  SECTION("test with semicolon in key") {
+    test = R"({use_sidepath,secondary,3,1,yes,50,"Rijksweg Noord",asphalt,left|through;right})";
+    values = psql_array_to_vector(test);
+    actual_values.push_back("use_sidepath");
+    actual_values.push_back("secondary");
+    actual_values.push_back("3");
+    actual_values.push_back("1");
+    actual_values.push_back("yes");
+    actual_values.push_back("50");
+    actual_values.push_back("Rijksweg Noord");
+    actual_values.push_back("asphalt");
+    actual_values.push_back("left|through;right");
+    REQUIRE (values == actual_values);
+  }
 }
