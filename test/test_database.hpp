d73ab0eeed46b8ba358c04e873809f76746c22d7
Florian Kargl <f.kargl@posteo.de>
2023-12-08 16:51:38 +0100

    Allow passing test database sql path as parameter

--- test/test_database.hpp
+++ test/test_database.hpp
@@ -12,6 +12,7 @@
 
 #include <stdexcept>
 #include <string>
+#include <filesystem>
 
 #include <fmt/core.h>
 
@@ -53,7 +54,7 @@ struct test_database {
   ~test_database();
 
   // create table structure and fill with fake data.
-  void setup();
+  void setup(const std::filesystem::path& sql_file = "test/structure.sql");
 
   // run a test. func will be called twice - once with each of a
   // writeable and readonly data selection available from the
@@ -95,7 +96,7 @@ private:
   static std::string random_db_name();
 
   // set up the schema of the database
-  static void setup_schema(pqxx::connection &w);
+  static void setup_schema(pqxx::connection &w, const std::filesystem::path& sql_file);
 
   // the name of the test database.
   std::string m_db_name;
