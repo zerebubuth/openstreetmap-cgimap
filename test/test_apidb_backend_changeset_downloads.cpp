fb8401f1edce1b38d52570d8d932c260bdd905f2
mmd-osm <mmd.osm@gmail.com>
2023-12-11 21:14:43 +0100

    Catch2 migration: test_apidb_backend_changeset_downloads

--- test/test_apidb_backend_changeset_downloads.cpp
+++ test/test_apidb_backend_changeset_downloads.cpp
@@ -27,54 +27,91 @@
 #include "test_database.hpp"
 #include "test_request.hpp"
 
-namespace {
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
 
-template <typename T>
-void assert_equal(const T& a, const T&b, const std::string &message) {
-  if (a != b) {
-    throw std::runtime_error(fmt::format("Expecting {} to be equal, but {} != {}", message, a, b));
+
+class DatabaseTestsFixture
+{
+protected:
+  DatabaseTestsFixture() = default;
+  static test_database tdb;
+};
+
+test_database DatabaseTestsFixture::tdb{};
+
+struct CGImapListener : Catch::TestEventListenerBase, DatabaseTestsFixture {
+
+    using TestEventListenerBase::TestEventListenerBase; // inherit constructor
+
+    void testRunStarting( Catch::TestRunInfo const& testRunInfo ) override {
+      // load database schema when starting up tests
+      tdb.setup();
+    }
+
+    void testCaseStarting( Catch::TestCaseInfo const& testInfo ) override {
+      tdb.testcase_starting();
+    }
+
+    void testCaseEnded( Catch::TestCaseStats const& testCaseStats ) override {
+      tdb.testcase_ended();
+    }
+};
+
+CATCH_REGISTER_LISTENER( CGImapListener )
+
+
+TEST_CASE_METHOD( DatabaseTestsFixture, "test_changeset_select_node", "[changeset][db]" ) {
+
+  auto sel = tdb.get_data_selection();
+
+  SECTION("Initialize test data") {
+
+    tdb.run_sql(
+      "INSERT INTO users (id, email, pass_crypt, creation_time, display_name, data_public) "
+      "VALUES "
+      "  (1, 'user_1@example.com', '', '2017-03-19T19:13:00Z', 'user_1', true); "
+
+      "INSERT INTO changesets (id, user_id, created_at, closed_at, num_changes) "
+      "VALUES "
+      "  (1, 1, '2017-03-19T19:13:00Z', '2017-03-19T19:13:00Z', 1);"
+
+      "INSERT INTO current_nodes (id, latitude, longitude, changeset_id, visible, \"timestamp\", tile, version) "
+      " VALUES "
+      "  (1, 90000000, 90000000, 1, true,  '2017-03-19T19:13:00Z', 3229120632, 1); "
+
+      "INSERT INTO nodes (node_id, latitude, longitude, changeset_id, visible, "
+      "                   \"timestamp\", tile, version, redaction_id) "
+      "VALUES "
+      "  (1, 90000000, 90000000, 1, true,  '2017-03-19T19:13:00Z', 3229120632, 1, NULL);"
+      );
+
+  }
+
+  SECTION("Perform node checks") {
+
+    int num = sel->select_historical_by_changesets({1});
+    REQUIRE(num == 1); // should have selected one element from changeset 1
+
+    test_formatter f;
+    sel->write_nodes(f);
+    REQUIRE(f.m_nodes.size() == 1); // should have written one node from changeset 1
+
+    REQUIRE(
+      test_formatter::node_t(
+        element_info(1, 1, 1, "2017-03-19T19:13:00Z", 1, std::string("user_1"), true),
+        9.0, 9.0,
+        tags_t()) == f.m_nodes.front()); // node 1 in changeset 1
+
   }
 }
 
-void test_changeset_select_node(test_database &tdb) {
-  tdb.run_sql(
-    "INSERT INTO users (id, email, pass_crypt, creation_time, display_name, data_public) "
-    "VALUES "
-    "  (1, 'user_1@example.com', '', '2017-03-19T19:13:00Z', 'user_1', true); "
+TEST_CASE_METHOD( DatabaseTestsFixture, "test_changeset_select_way", "[changeset][db]" ) {
 
-    "INSERT INTO changesets (id, user_id, created_at, closed_at, num_changes) "
-    "VALUES "
-    "  (1, 1, '2017-03-19T19:13:00Z', '2017-03-19T19:13:00Z', 1);"
-
-    "INSERT INTO current_nodes (id, latitude, longitude, changeset_id, visible, \"timestamp\", tile, version) "
-    " VALUES "
-    "  (1, 90000000, 90000000, 1, true,  '2017-03-19T19:13:00Z', 3229120632, 1); "
-
-    "INSERT INTO nodes (node_id, latitude, longitude, changeset_id, visible, "
-    "                   \"timestamp\", tile, version, redaction_id) "
-    "VALUES "
-    "  (1, 90000000, 90000000, 1, true,  '2017-03-19T19:13:00Z', 3229120632, 1, NULL);"
-    );
   auto sel = tdb.get_data_selection();
 
-  int num = sel->select_historical_by_changesets({1});
-  assert_equal<int>(num, 1, "should have selected one element from changeset 1");
+  SECTION("Initialize test data") {
 
-  test_formatter f;
-  sel->write_nodes(f);
-  assert_equal<size_t>(f.m_nodes.size(), 1,
-    "should have written one node from changeset 1");
-
-  assert_equal<test_formatter::node_t>(
-    f.m_nodes.front(),
-    test_formatter::node_t(
-      element_info(1, 1, 1, "2017-03-19T19:13:00Z", 1, std::string("user_1"), true),
-      9.0, 9.0,
-      tags_t()),
-    "node 1 in changeset 1");
-}
-
-void test_changeset_select_way(test_database &tdb) {
   tdb.run_sql(
     "INSERT INTO users (id, email, pass_crypt, creation_time, display_name, data_public) "
     "VALUES "
@@ -105,172 +142,147 @@ void test_changeset_select_way(test_database &tdb) {
     "  (1, 2, 1, 1), "
     "  (1, 1, 1, 1); "
     );
-  auto sel = tdb.get_data_selection();
+  }
 
-  int num = sel->select_historical_by_changesets({2});
-  assert_equal<int>(num, 2, "number of ways (2) selected from changeset 1");
+  SECTION("Perform way checks") {
 
-  test_formatter f;
-  sel->write_ways(f);
-  assert_equal<size_t>(f.m_ways.size(), 2,
-    "number of ways (2) written from changeset 1");
-
-  assert_equal<test_formatter::way_t>(
-    f.m_ways[0],
-    test_formatter::way_t(
-      element_info(1, 1, 2, "2017-03-19T19:57:00Z", 1, std::string("user_1"), true),
-      nodes_t{1},
-      tags_t()),
-    "way 1, version 1 in changeset 1");
-  assert_equal<test_formatter::way_t>(
-    f.m_ways[1],
-    test_formatter::way_t(
-      element_info(1, 2, 2, "2017-03-19T19:57:00Z", 1, std::string("user_1"), true),
-      nodes_t{1},
-      tags_t()),
-    "way 1, version 2 in changeset 1");
-}
-
-void test_changeset_select_relation(test_database &tdb) {
-  tdb.run_sql(
-    "INSERT INTO users (id, email, pass_crypt, creation_time, display_name, data_public) "
-    "VALUES "
-    "  (1, 'user_1@example.com', '', '2017-03-19T20:15:00Z', 'user_1', true); "
-
-    "INSERT INTO changesets (id, user_id, created_at, closed_at, num_changes) "
-    "VALUES "
-    "  (1, 1, '2017-03-19T19:13:00Z', '2017-03-19T20:15:00Z', 1),"
-    "  (2, 1, '2017-03-19T19:13:00Z', '2017-03-19T20:15:00Z', 1);"
-
-    "INSERT INTO current_nodes (id, latitude, longitude, changeset_id, visible, \"timestamp\", tile, version) "
-    " VALUES "
-    "  (1, 90000000, 90000000, 1, true,  '2017-03-19T20:15:00Z', 3229120632, 1); "
-
-    "INSERT INTO nodes (node_id, latitude, longitude, changeset_id, visible, "
-    "                   \"timestamp\", tile, version, redaction_id) "
-    "VALUES "
-    "  (1, 90000000, 90000000, 1, true,  '2017-03-19T20:15:00Z', 3229120632, 1, NULL);"
-
-    "INSERT INTO relations (relation_id, changeset_id, \"timestamp\", "
-    "                       visible, version, redaction_id) "
-    "VALUES "
-    "  (1, 2, '2017-03-19T20:15:00Z', true, 1, NULL); "
-
-    "INSERT INTO relation_members (relation_id, member_type, member_id, "
-    "                              member_role, sequence_id, version) "
-    "VALUES "
-    "  (1, 'Node', 1, 'foo', 1, 1); "
-    );
-  auto sel = tdb.get_data_selection();
-
-  int num = sel->select_historical_by_changesets({2});
-  assert_equal<int>(num, 1, "number of relations (1) selected from changeset 1");
-
-  test_formatter f;
-  sel->write_relations(f);
-  assert_equal<size_t>(f.m_relations.size(), 1,
-    "number of relations (1) written from changeset 1");
-
-  assert_equal<test_formatter::relation_t>(
-    f.m_relations.front(),
-    test_formatter::relation_t(
-      element_info(1, 1, 2, "2017-03-19T20:15:00Z", 1, std::string("user_1"), true),
-      members_t{member_info(element_type_node, 1, "foo")},
-      tags_t()),
-    "relation 1, version 1 in changeset 1");
-}
-
-void test_changeset_redacted(test_database &tdb) {
-  tdb.run_sql(
-    "INSERT INTO users (id, email, pass_crypt, creation_time, display_name, data_public) "
-    "VALUES "
-    "  (1, 'user_1@example.com', '', '2017-03-19T20:18:00Z', 'user_1', true); "
-
-    "INSERT INTO changesets (id, user_id, created_at, closed_at, num_changes) "
-    "VALUES "
-    "  (1, 1, '2017-03-19T20:18:00Z', '2017-03-19T20:18:00Z', 1),"
-    "  (2, 1, '2017-03-19T20:18:00Z', '2017-03-19T20:18:00Z', 1),"
-    "  (3, 1, '2017-03-19T20:18:00Z', '2017-03-19T20:18:00Z', 1);"
-
-    "INSERT INTO redactions (id, title, created_at, updated_at, user_id) "
-    "VALUES "
-    "  (1, 'test redaction', '2017-03-19T20:18:00Z', '2017-03-19T20:18:00Z', 1); "
-
-    "INSERT INTO current_nodes (id, latitude, longitude, changeset_id, visible, \"timestamp\", tile, version) "
-    " VALUES "
-    "  (1, 0, 0, 3, true,  '2017-03-19T20:18:00Z', 3221225472, 3); "
-
-    "INSERT INTO nodes (node_id, latitude, longitude, changeset_id, visible, "
-    "                   \"timestamp\", tile, version, redaction_id) "
-    "VALUES "
-    "  (1, 0, 0, 1, true,  '2017-03-19T20:18:00Z', 3221225472, 1, NULL),"
-    "  (1, 0, 0, 2, true,  '2017-03-19T20:18:00Z', 3221225472, 2, 1),"
-    "  (1, 0, 0, 3, true,  '2017-03-19T20:18:00Z', 3221225472, 3, NULL);"
-    );
-  auto sel = tdb.get_data_selection();
-
-  {
     int num = sel->select_historical_by_changesets({2});
-    assert_equal<int>(num, 0, "number of elements (0) selected by regular user from changeset 2");
+    REQUIRE(num == 2); // number of ways (2) selected from changeset 1
 
     test_formatter f;
-    sel->write_nodes(f);
-    assert_equal<size_t>(f.m_nodes.size(), 0,
-      "number of nodes (0) written for regular user from changeset 2");
+    sel->write_ways(f);
+    REQUIRE(f.m_ways.size() == 2); // number of ways (2) written from changeset 1
+
+    REQUIRE(
+        test_formatter::way_t(
+        element_info(1, 1, 2, "2017-03-19T19:57:00Z", 1, std::string("user_1"), true),
+        nodes_t{1},
+        tags_t()) == f.m_ways[0]); // way 1, version 1 in changeset 1
+
+    REQUIRE(
+        test_formatter::way_t(
+        element_info(1, 2, 2, "2017-03-19T19:57:00Z", 1, std::string("user_1"), true),
+        nodes_t{1},
+        tags_t()) == f.m_ways[1]); // way 1, version 2 in changeset 1
+  }
+}
+
+TEST_CASE_METHOD( DatabaseTestsFixture, "test_changeset_select_relation", "[changeset][db]" ) {
+
+  auto sel = tdb.get_data_selection();
+
+  SECTION("Initialize test data") {
+
+    tdb.run_sql(
+      "INSERT INTO users (id, email, pass_crypt, creation_time, display_name, data_public) "
+      "VALUES "
+      "  (1, 'user_1@example.com', '', '2017-03-19T20:15:00Z', 'user_1', true); "
+
+      "INSERT INTO changesets (id, user_id, created_at, closed_at, num_changes) "
+      "VALUES "
+      "  (1, 1, '2017-03-19T19:13:00Z', '2017-03-19T20:15:00Z', 1),"
+      "  (2, 1, '2017-03-19T19:13:00Z', '2017-03-19T20:15:00Z', 1);"
+
+      "INSERT INTO current_nodes (id, latitude, longitude, changeset_id, visible, \"timestamp\", tile, version) "
+      " VALUES "
+      "  (1, 90000000, 90000000, 1, true,  '2017-03-19T20:15:00Z', 3229120632, 1); "
+
+      "INSERT INTO nodes (node_id, latitude, longitude, changeset_id, visible, "
+      "                   \"timestamp\", tile, version, redaction_id) "
+      "VALUES "
+      "  (1, 90000000, 90000000, 1, true,  '2017-03-19T20:15:00Z', 3229120632, 1, NULL);"
+
+      "INSERT INTO relations (relation_id, changeset_id, \"timestamp\", "
+      "                       visible, version, redaction_id) "
+      "VALUES "
+      "  (1, 2, '2017-03-19T20:15:00Z', true, 1, NULL); "
+
+      "INSERT INTO relation_members (relation_id, member_type, member_id, "
+      "                              member_role, sequence_id, version) "
+      "VALUES "
+      "  (1, 'Node', 1, 'foo', 1, 1); "
+      );
+
   }
 
-  // as a moderator, should have all redacted elements shown.
-  sel->set_redactions_visible(true);
-  {
+  SECTION("Perform relation checks") {
+
     int num = sel->select_historical_by_changesets({2});
-    assert_equal<int>(num, 1, "number of elements (1) selected by moderator from changeset 2");
+    REQUIRE(num == 1); // number of relations (1) selected from changeset 1
 
     test_formatter f;
-    sel->write_nodes(f);
-    assert_equal<size_t>(f.m_nodes.size(), 1,
-      "number of nodes (1) written for moderator from changeset 2");
+    sel->write_relations(f);
+    REQUIRE(f.m_relations.size() == 1); // number of relations (1) written from changeset 1
 
-    assert_equal<test_formatter::node_t>(
-      f.m_nodes.front(),
-      test_formatter::node_t(
-        element_info(1, 2, 2, "2017-03-19T20:18:00Z", 1, std::string("user_1"), true),
-        0.0, 0.0,
-        tags_t()),
-      "redacted node 1 in changeset 2");
+    REQUIRE(
+      test_formatter::relation_t(
+        element_info(1, 1, 2, "2017-03-19T20:15:00Z", 1, std::string("user_1"), true),
+        members_t{member_info(element_type_node, 1, "foo")},
+        tags_t()) == f.m_relations.front());  // relation 1, version 1 in changeset 1
   }
 }
 
-} // anonymous namespace
+TEST_CASE_METHOD( DatabaseTestsFixture, "test_changeset_redacted", "[changeset][db]" ) {
 
-int main(int, char **) {
-  try {
-    test_database tdb;
-    tdb.setup();
+  auto sel = tdb.get_data_selection();
 
-    tdb.run(std::function<void(test_database&)>(
-        &test_changeset_select_node));
+  SECTION("Initialize test data") {
 
-    tdb.run(std::function<void(test_database&)>(
-        &test_changeset_select_way));
+    tdb.run_sql(
+      "INSERT INTO users (id, email, pass_crypt, creation_time, display_name, data_public) "
+      "VALUES "
+      "  (1, 'user_1@example.com', '', '2017-03-19T20:18:00Z', 'user_1', true); "
 
-    tdb.run(std::function<void(test_database&)>(
-        &test_changeset_select_relation));
+      "INSERT INTO changesets (id, user_id, created_at, closed_at, num_changes) "
+      "VALUES "
+      "  (1, 1, '2017-03-19T20:18:00Z', '2017-03-19T20:18:00Z', 1),"
+      "  (2, 1, '2017-03-19T20:18:00Z', '2017-03-19T20:18:00Z', 1),"
+      "  (3, 1, '2017-03-19T20:18:00Z', '2017-03-19T20:18:00Z', 1);"
 
-    tdb.run(std::function<void(test_database&)>(
-        &test_changeset_redacted));
+      "INSERT INTO redactions (id, title, created_at, updated_at, user_id) "
+      "VALUES "
+      "  (1, 'test redaction', '2017-03-19T20:18:00Z', '2017-03-19T20:18:00Z', 1); "
 
-  } catch (const test_database::setup_error &e) {
-    std::cout << "Unable to set up test database: " << e.what() << std::endl;
-    return 77;
+      "INSERT INTO current_nodes (id, latitude, longitude, changeset_id, visible, \"timestamp\", tile, version) "
+      " VALUES "
+      "  (1, 0, 0, 3, true,  '2017-03-19T20:18:00Z', 3221225472, 3); "
 
-  } catch (const std::exception &e) {
-    std::cout << "Error: " << e.what() << std::endl;
-    return 1;
-
-  } catch (...) {
-    std::cout << "Unknown exception type." << std::endl;
-    return 99;
+      "INSERT INTO nodes (node_id, latitude, longitude, changeset_id, visible, "
+      "                   \"timestamp\", tile, version, redaction_id) "
+      "VALUES "
+      "  (1, 0, 0, 1, true,  '2017-03-19T20:18:00Z', 3221225472, 1, NULL),"
+      "  (1, 0, 0, 2, true,  '2017-03-19T20:18:00Z', 3221225472, 2, 1),"
+      "  (1, 0, 0, 3, true,  '2017-03-19T20:18:00Z', 3221225472, 3, NULL);"
+      );
   }
 
-  return 0;
+  SECTION("Perform redaction checks") {
+
+    {
+      int num = sel->select_historical_by_changesets({2});
+      REQUIRE(num == 0); // number of elements (0) selected by regular user from changeset 2
+
+      test_formatter f;
+      sel->write_nodes(f);
+      REQUIRE(f.m_nodes.size() == 0); // number of nodes (0) written for regular user from changeset 2
+    }
+
+    // as a moderator, should have all redacted elements shown.
+    sel->set_redactions_visible(true);
+    {
+      int num = sel->select_historical_by_changesets({2});
+      REQUIRE(num ==  1);  // number of elements (1) selected by moderator from changeset 2
+
+      test_formatter f;
+      sel->write_nodes(f);
+      REQUIRE(f.m_nodes.size() == 1); // number of nodes (1) written for moderator from changeset 2
+
+      REQUIRE(
+        test_formatter::node_t(
+          element_info(1, 2, 2, "2017-03-19T20:18:00Z", 1, std::string("user_1"), true),
+          0.0, 0.0,
+          tags_t()) == f.m_nodes.front()); // redacted node 1 in changeset 2
+    }
+  }
 }
+
