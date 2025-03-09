197f20170582666dc4490d5deefeea1d31daf15d
mmd-osm <mmd.osm@gmail.com>
2025-03-09 19:23:28 +0000

    [test] Move test_core helper functions to new file

---
+++ test/test_core_helper.hpp
@@ -0,0 +1,23 @@
+/**
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * This file is part of openstreetmap-cgimap (https://github.com/zerebubuth/openstreetmap-cgimap/).
+ *
+ * Copyright (C) 2009-2024 by the CGImap developer community.
+ * For a full list of authors see the git log.
+ */
+
+#ifndef TEST_TEST_CORE_HELPER_HPP
+#define TEST_TEST_CORE_HELPER_HPP
+
+#include "test_request.hpp"
+#include "test_types.hpp"
+
+namespace fs = std::filesystem;
+
+void setup_request_headers(test_request &req, std::istream &in);
+void check_response(std::istream &expected, std::istream &actual);
+oauth2_tokens get_oauth2_tokens(const fs::path &oauth2_file);
+user_roles_t get_user_roles(const fs::path &roles_file);
+
+#endif
