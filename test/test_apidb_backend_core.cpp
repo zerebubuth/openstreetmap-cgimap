1b31d655b8826f40948e70dc164c7e80bca34b95
mmd-osm <mmd.osm@gmail.com>
2025-03-11 21:38:46 +0000

    Baseline test_apidb_backend_core

--- test/test_apidb_backend_core.cpp
+++ test/test_apidb_backend_core.cpp
@@ -10,10 +10,12 @@
 #include "cgimap/process_request.hpp"
 #include "cgimap/time.hpp"
 #include "cgimap/util.hpp"
-#include "staticxml.hpp"
 
+#include <stdexcept>
 #include <fmt/core.h>
 
+#include <sys/time.h>
+#include <cstdio>
 #include <filesystem>
 #include <fstream>
 #include <vector>
@@ -22,11 +24,56 @@
 
 #include "test_core_helper.hpp"
 #include "test_request.hpp"
+#include "xmlparser.hpp"
+#include "test_apidb_importer.hpp"
+
+#include "cgimap/backend/apidb/transaction_manager.hpp"
+
+#include "test_formatter.hpp"
+#include "test_database.hpp"
 
 #define CATCH_CONFIG_RUNNER
 #include <catch2/catch.hpp>
 
-namespace fs = std::filesystem;
+namespace {
+
+class DatabaseTestsFixture
+{
+public:
+  static void setTestDatabaseSchema(const std::filesystem::path& db_sql) {
+    test_db_sql = db_sql;
+  }
+
+protected:
+  DatabaseTestsFixture() = default;
+  inline static std::filesystem::path test_db_sql{"test/structure.sql"};
+  static test_database tdb;
+};
+
+test_database DatabaseTestsFixture::tdb{};
+
+struct CGImapListener : Catch::TestEventListenerBase, DatabaseTestsFixture {
+
+    using TestEventListenerBase::TestEventListenerBase; // inherit constructor
+
+    void testRunStarting( Catch::TestRunInfo const& testRunInfo ) override {
+      // load database schema when starting up tests
+      tdb.setup(test_db_sql);
+    }
+
+    void testCaseStarting( Catch::TestCaseInfo const& testInfo ) override {
+      tdb.testcase_starting();
+    }
+
+    void testCaseEnded( Catch::TestCaseStats const& testCaseStats ) override {
+      tdb.testcase_ended();
+    }
+};
+
+CATCH_REGISTER_LISTENER( CGImapListener )
+
+} // anonymous namespace
+
 
 fs::path test_directory{};
 
@@ -42,16 +89,14 @@ std::vector<fs::path> get_test_cases() {
   return test_cases;
 }
 
-TEST_CASE("Execute core test cases using external test data") {
+TEST_CASE_METHOD( DatabaseTestsFixture, "readonly_pgsql core", "[core][db]" ) {
 
   user_roles_t user_roles;
   oauth2_tokens oauth2_tokens;
   null_rate_limiter limiter;
   routes route;
-  po::variables_map vm;
 
-  SECTION("Execute test cases") {
-    // Note: Current Catch2 version does not support test cases with dynamic names
+  SECTION("Initialize test data") {
 
     if (test_directory.empty()) {
       FAIL("No test directory specified. Missing --test-directory command line option.");
@@ -72,16 +117,25 @@ TEST_CASE("Execute core test cases using external test data") {
     REQUIRE_NOTHROW(user_roles = get_user_roles(roles_file));
     REQUIRE_NOTHROW(oauth2_tokens = get_oauth2_tokens(oauth2_file));
 
+    auto database = parse_xml(data_file.c_str());
+
+    auto txn = tdb.get_data_update_factory()->get_default_transaction();
+    auto m = Transaction_Manager(*txn);
+
+    populate_database(m, *database, user_roles, oauth2_tokens);
+
+    m.commit();
+  }
+
+  SECTION("Execute test cases") {
+    // Note: Current Catch2 version does not support test cases with dynamic names
+
+    auto sel_factory = tdb.get_data_selection_factory();
+
     auto test_cases = get_test_cases();
     if(test_cases.empty()) {
       FAIL("No test cases found in the test directory.");
     }
-
-    // Prepare the backend with the test data
-    vm.try_emplace("file", po::variable_value(data_file.native(), false));
-
-    auto data_backend = make_staticxml_backend(user_roles, oauth2_tokens);
-    auto factory = data_backend->create(vm);
 
     // Execute actual test cases
     for (fs::path test_case : test_cases) {
@@ -95,7 +149,7 @@ TEST_CASE("Execute core test cases using external test data") {
       setup_request_headers(req, in);
 
       // execute the request
-      REQUIRE_NOTHROW(process_request(req, limiter, generator, route, *factory, nullptr));
+      REQUIRE_NOTHROW(process_request(req, limiter, generator, route, *sel_factory, nullptr));
 
       CAPTURE(req.body().str());
       REQUIRE_NOTHROW(check_response(in, req.buffer()));
@@ -103,23 +157,31 @@ TEST_CASE("Execute core test cases using external test data") {
   }
 }
 
-
 int main(int argc, char *argv[]) {
   Catch::Session session;
+
+  std::filesystem::path test_db_sql{ "test/structure.sql" };
 
   using namespace Catch::clara;
   auto cli =
       session.cli()
+      | Opt(test_db_sql,
+            "db-schema")
+            ["--db-schema"]
+      ("test database schema file")
       | Opt(test_directory,
-            "test-directory")
-            ["--test-directory"]
-      ("test case directory");
+        "test-directory")
+        ["--test-directory"]
+        ("test case directory");
 
   session.cli(cli);
 
   int returnCode = session.applyCommandLine(argc, argv);
   if (returnCode != 0)
     return returnCode;
+
+  if (!test_db_sql.empty())
+    DatabaseTestsFixture::setTestDatabaseSchema(test_db_sql);
 
   return session.run();
 }
